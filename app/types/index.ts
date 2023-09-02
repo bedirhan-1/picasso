a290f402dc76ff4420cda5ffce65f24025b86d83
bedirhan-1 <bedirhan9828@hotmail.com>
2023-09-02 19:04:10 +0300

    feat: No user version

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,10 +1,3 @@
-import { User } from "@prisma/client";
-
-export type SafeUser = Omit<User, "createdAt" | "updatedAt"> & {
-  createdAt: string;
-  updatedAt: string;
-};
-
 export type ISlider = {
   Image: IImage[];
 };
